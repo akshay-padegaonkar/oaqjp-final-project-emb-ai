858ed7f22c3d3a62bb5b2b2ebfc27f82c8a8fb0d
akshay-padegaonkar <akshay.padegaonkar@gmail.com>
2026-03-30 18:22:22 -0400

    modified test code

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,16 +1,28 @@
 from EmotionDetection import emotion_detector
 
-text1 = "I am glad this happened"
-text2 = "I am really mad about this"
-text3 = "I feel disgusted just hearing about this"
-text4 = "I am so sad about this"
-text5 = "I am really afraid that this will happen"
+texts = [
+    "I am glad this happened",
+    "I am really mad about this",
+    "I feel disgusted just hearing about this",
+    "I am so sad about this",
+    "I am really afraid that this will happen"
+]
+#text1 = "I am glad this happened"
+#text2 = "I am really mad about this"
+#text3 = "I feel disgusted just hearing about this"
+#text4 = "I am so sad about this"
+#text5 = "I am really afraid that this will happen"
+
+for t in texts:
+    results = emotion_detector(t)
+    dominant = results.get("dominant_emotion","")
+
 
 result1 = emotion_detector(text1)
 dominant1 = result1.get("dominant_emotion", "")
-print(dominant1)
+print(text1,"->", dominant1)
 
 result2 = emotion_detector(text2)
 dominant2 = result2.get("dominant_emotion","")
-print(dominant2)
+print(text2, dominant2)
 
