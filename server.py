e15264905b6515dcceccc94419e565b0a14b2864
akshay-padegaonkar <53310450+akshay-padegaonkar@users.noreply.github.com>
2026-03-31 10:10:24 -0400

    Update server.py

--- server.py
+++ server.py
@@ -12,6 +12,8 @@ def detect_emotion():
     data = request.get_json()
     text = data.get("text","")
     result = emotion_detector(text)
+    if result["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
     return jsonify(result)
 
 if __name__ == "__main__":
